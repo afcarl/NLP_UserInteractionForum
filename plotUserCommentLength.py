768a58d18a3730b9011335178c7e558107707465
Benni <ben.athiwaratkun@gmail.com>
2014-09-12 00:03:18 -0400

    Conditional Plots [Complete]

--- plotUserCommentLength.py
+++ plotUserCommentLength.py
@@ -2,9 +2,9 @@ import numpy as np
 import userCommentLength
 import matplotlib.pyplot as plt
 import math
+import userCommentLength;
 
-def main():
-    import userCommentLength;
+def plotHists():
     textLenHistograms = userCommentLength.loadTextLenHistograms();
     for user in textLenHistograms:
         #print user
@@ -20,6 +20,33 @@ def main():
         ax.plot(hist[1][0:-1], hist[0]);
         plt.show()
         
+def plotCondHists(numFriends):
+    conds = userCommentLength.loadCondTextLenHistograms();
+    activeUsers = userCommentLength.loadMostActiveUsers(numFriends);
+    for user in activeUsers:
+        fig = plt.figure();
+        fig.suptitle("Conditional Histograms of User " + user);
+        axs = np.array([])
+        for i in range(numFriends):
+            sq = int(math.sqrt(numFriends))
+            axs = np.append( axs,  fig.add_subplot(sq,sq,i+1))
+        #ax1 = fig.add_subplot(331);
+        #ax2 = fig.add_subplot(332);
+        counter = 0;
+        fig.subplots_adjust(top=0.85)
+        for pair in conds:
+            if pair[0] == user:
+                user2 = pair[1];
+                seq = np.array([int(math.sqrt(num))for num in conds[pair]])
+                maxVal = max(seq)
+                hist = np.histogram(seq, bins=range(0, maxVal, 2))
+                axs[counter].plot(hist[1][0:-1], hist[0]);
+                axs[counter].set_ylabel(user2)
+                counter += 1;
+                #ax1.plot()
+        plt.show();
+        
 
 if __name__ == "__main__":
-    main();
+    #plotHists();
+    plotCondHists(25);
--- userCommentLength.py
+++ userCommentLength.py
@@ -1,5 +1,6 @@
 import numpy as np
 import projectUtil
+import pickle
 
 def getMostActiveUsers(ts, numActiveUsers=-1):
     users = {}; # will store the number of comments posted
@@ -19,7 +20,8 @@ def getMostActiveUsers(ts, numActiveUsers=-1):
     else:
         return np.array([u[0] for u in sortedUsers[-numActiveUsers:]]);
 
-def constructHistograms(ts, users):
+def constructHistograms(ts, numUsers):
+    users = loadMostActiveUsers(numUsers);
     setUsers = set(users); # for fast membership test
     userHistograms = {};
     for thread in ts:
@@ -34,29 +36,66 @@ def constructHistograms(ts, users):
     
     
     
-def constructConditionalHistograms(ts, users):
-    setUsers = set(users);
+def constructConditionalHistograms(ts, numUsers):
+    users = loadMostActiveUsers(numUsers);
+    print "Start Constructing Conditional Histogram"
     condHistograms = {};
-    #import pickle
-    #pickle.dump(condHistograms, open('Data/small_userTextLenConditionalHistogram.p','wb'))
+    for user1 in users:
+        for user2 in users:
+            # if not (user1 == user2):
+            print "."
+            for thread in ts:
+                if(threadContainsBothUsers(thread, user1, user2)):
+                    # count number of user1's comment
+                    for comment in thread:
+                        if comment['author'] == user1:
+                            if (user1, user2) in condHistograms:
+                                condHistograms[(user1, user2)] = np.append(condHistograms[(user1, user2)], comment['textLen'])
+                            else:
+                                condHistograms[(user1, user2)] = np.array([comment['textLen']])
+    import pickle
+    pickle.dump(condHistograms, open('Data/userTextLenConditionalHistogram.p','wb'))
     
+def threadContainsBothUsers(thread, user1, user2):
+    user1_in = False;
+    user2_in = False
+    for comment in thread:
+        if comment['author'] == user1:
+            user1_in = True;
+        if comment['author'] == user2:
+            user2_in = True;
+        if user1_in and user2_in:
+            break;
+    return (user1_in and user2_in)
+
+
 def constructHistogramsConditional(ts, users):
     pass;
 
 def loadUserNumComment():
-    import pickle
     return pickle.load(open('Data/sortedUsersNumComments.p','rb'));
 
 def loadTextLenHistograms():
-    import pickle
     return pickle.load(open('Data/userTextLenHistogram.p','rb'));
+
+def loadCondTextLenHistograms():
+    return pickle.load(open('Data/userTextLenConditionalHistogram.p','rb'))
+
+def loadMostActiveUsers(numUsers= -1):
+    allUsers = loadUserNumComment();
+    if numUsers == -1:
+        return np.array([u[0] for u in allUsers])
+    else:
+        return np.array([u[0] for u in allUsers[-numUsers:]])
+    
 
 def main():
     ts = projectUtil.loadData();
     # First: find 100 most active users (counting the number of comments)
-    mostActiveUsers = getMostActiveUsers(ts,10);
-    print mostActiveUsers;
-    constructHistograms(ts, mostActiveUsers);
+    #mostActiveUsers = getMostActiveUsers(ts,10);
+    #print mostActiveUsers;
+    #constructHistograms(ts, mostActiveUsers);
+    constructConditionalHistograms(ts, 25)
 
 
 if __name__ == "__main__":
